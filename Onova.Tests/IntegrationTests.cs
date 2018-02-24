70c37153d1c57f3618c4c62b7018ffd384282b82
Alexey Golub <Tyrrrrrr@gmail.com>
2018-02-24 18:57:23 +0200

    Cleanup integration tests

--- Onova.Tests/IntegrationTests.cs
+++ Onova.Tests/IntegrationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Onova.Services;
@@ -9,12 +10,29 @@ namespace Onova.Tests
     [TestFixture]
     public class IntegrationTests
     {
+        private static string TestDirPath => TestContext.CurrentContext.TestDirectory;
+        private static string TempDirPath => Path.Combine(TestDirPath, "Temp");
+
+        private static string StorageDirPath => Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "Onova",
+            Assembly.GetExecutingAssembly().GetName().Name);
+
+        [TearDown]
+        public void Cleanup()
+        {
+            if (Directory.Exists(StorageDirPath))
+                Directory.Delete(StorageDirPath, true);
+            if (Directory.Exists(TempDirPath))
+                Directory.Delete(TempDirPath, true);
+        }
+
         [Test]
         public async Task GithubPackageResolver_GetAllVersionsAsync_Test()
         {
-            var resolver = new GithubPackageResolver("Tyrrrz", "OnovaTestRepo", "Test.onv");
-
             var expectedVersions = new[] {Version.Parse("1.0"), Version.Parse("2.0")};
+
+            var resolver = new GithubPackageResolver("Tyrrrz", "OnovaTestRepo", "Test.onv");
             var versions = await resolver.GetAllVersionsAsync();
 
             Assert.That(versions, Is.Not.Null);
@@ -24,8 +42,9 @@ namespace Onova.Tests
         [Test]
         public async Task GithubPackageResolver_GetPackageAsync_Test()
         {
-            var resolver = new GithubPackageResolver("Tyrrrz", "OnovaTestRepo", "Test.onv");
+            const string expectedContent = "Hello world";
 
+            var resolver = new GithubPackageResolver("Tyrrrz", "OnovaTestRepo", "Test.onv");
             var stream = await resolver.GetPackageAsync(Version.Parse("2.0"));
 
             Assert.That(stream, Is.Not.Null);
@@ -34,7 +53,7 @@ namespace Onova.Tests
             using (var reader = new StreamReader(stream))
             {
                 var content = await reader.ReadToEndAsync();
-                Assert.That(content, Is.EqualTo("Hello world"));
+                Assert.That(content, Is.EqualTo(expectedContent));
             }
         }
     }

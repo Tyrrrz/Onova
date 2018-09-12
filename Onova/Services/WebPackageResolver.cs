db8512b4f08e84274fec2023a836793bd2f82e94
Felipe Martínez <pipernene@gmail.com>
2018-09-12 21:14:29 +0200

    Allow relative urls in manifest in WebPackageResolver (#15)

--- Onova/Services/WebPackageResolver.cs
+++ Onova/Services/WebPackageResolver.cs
@@ -57,6 +57,9 @@ namespace Onova.Services
                 if (!Version.TryParse(versionText, out var version))
                     continue;
 
+                // Convert to Uri and back to account for relative paths, etc
+                url = new Uri(new Uri(_manifestUrl), url).ToString();
+
                 // Add to dictionary
                 map[version] = url;
             }

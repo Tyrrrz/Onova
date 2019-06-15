38ec85242842e5944ff7d966ceaffe68e8c14f50
Alexey Golub <Tyrrrrrr@gmail.com>
2019-06-15 17:51:34 +0300

    Fix recursive directory copy in Onova.Updater

--- Onova.Updater/Internal/DirectoryEx.cs
+++ Onova.Updater/Internal/DirectoryEx.cs
@@ -8,25 +8,27 @@ namespace Onova.Updater.Internal
         {
             Directory.CreateDirectory(destDirPath);
 
-            // Get all subdirectories in source directory
-            var sourceSubDirPaths = Directory.EnumerateDirectories(sourceDirPath, "*", SearchOption.TopDirectoryOnly);
-
-            // Recursively copy them
-            foreach (var sourceSubDirPath in sourceSubDirPaths)
-            {
-                var destSubDirPath = Path.Combine(destDirPath, Path.GetDirectoryName(sourceSubDirPath));
-                Copy(sourceSubDirPath, destSubDirPath, overwrite);
-            }
-
             // Get all files in source directory
-            var sourceFilePaths = Directory.EnumerateFiles(sourceDirPath, "*", SearchOption.TopDirectoryOnly);
+            var sourceFilePaths = Directory.EnumerateFiles(sourceDirPath);
 
             // Copy them
             foreach (var sourceFilePath in sourceFilePaths)
             {
                 // Get destination file path
-                var destFilePath = Path.Combine(destDirPath, Path.GetFileName(sourceFilePath));
+                var destFileName = Path.GetFileName(sourceFilePath);
+                var destFilePath = Path.Combine(destDirPath, destFileName);
                 File.Copy(sourceFilePath, destFilePath, overwrite);
+            }
+
+            // Get all subdirectories in source directory
+            var sourceSubDirPaths = Directory.EnumerateDirectories(sourceDirPath);
+
+            // Recursively copy them
+            foreach (var sourceSubDirPath in sourceSubDirPaths)
+            {
+                var destSubDirName = Path.GetFileName(sourceSubDirPath);
+                var destSubDirPath = Path.Combine(destDirPath, destSubDirName);
+                Copy(sourceSubDirPath, destSubDirPath, overwrite);
             }
         }
     }
